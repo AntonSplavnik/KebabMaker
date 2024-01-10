2f2a118153d143572cba8b527ab6bd94ce86a992
Anton Splavnik <antonsplavnik@gmail.com>
2024-01-10 22:55:06 +0100

    added smothe rotation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public AudioSource audioSource;
-
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] float rotationSpeed = 5f;
     private Rigidbody2D rb;
 
     void Start()
@@ -15,18 +15,36 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+        
         Vector2 movement = new Vector2(horizontalInput, verticalInput);
         movement.Normalize();
-        rb.velocity = new Vector2(movement.x * moveSpeed, movement.y * moveSpeed);
-    
-        Vector3 movementDirection = new Vector3(verticalInput, horizontalInput, 0f).normalized;
-        if (movementDirection != Vector3.zero)
+        // transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
+        //
+        // if (movement != Vector2.zero)
+        //     transform.forward = movement;
+
+        rb.position += movement * moveSpeed * Time.deltaTime;
+
+        if (movement != Vector2.zero)
         {
-            float angle = Mathf.Atan2(movementDirection.y, movementDirection.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            // Calculate the rotation angle based on the movement direction
+            float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
+            // Create a rotation quaternion
+            Quaternion toRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            // Smoothly rotate towards the target rotation
+            transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+            // float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
+            // rb.rotation = angle;
         }
+        
+        // rb.velocity = new Vector2(movement.x * moveSpeed, movement.y * moveSpeed);
+        // Vector3 movementDirection = new Vector3(verticalInput, horizontalInput, 0f).normalized;
+        // if (movementDirection != Vector3.zero)
+        // {
+        //     float angle = Mathf.Atan2(movementDirection.y, movementDirection.x) * Mathf.Rad2Deg;
+        //     transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        // }
     }
 }
