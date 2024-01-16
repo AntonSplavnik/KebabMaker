797288c3155bbf3ff4652e1a7dc5e6164c4bbd09
dantol29 <dantol29@gmail.com>
2024-01-16 15:25:27 -0800

    customer animation

--- Assets/Scripts/CustomerMovement.cs
+++ Assets/Scripts/CustomerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,7 @@ using UnityEngine;
 public class CustomerMovement1 : MonoBehaviour
 {
     [SerializeField] private GameObject customer;
+    public GameObject sittingCustomer;
     private Animator animator;
 
     void Start()
@@ -12,16 +14,16 @@ public class CustomerMovement1 : MonoBehaviour
         animator = GetComponent<Animator>();
         Invoke("PlayAnimation", 5f);
     }
-    
-    void FixedUpdate()
-    {
-        customer.SetActive(true);
-        
-    }
-    
+
     void PlayAnimation()
     {
         // Trigger the "YourAnimation" animation
         animator.SetTrigger("YourAnimation");
+    }
+
+    void OnAnimationEnd()
+    {
+        customer.SetActive(false);
+        sittingCustomer.SetActive(true);
     }
 }
